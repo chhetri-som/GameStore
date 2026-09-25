b926548966ceadc9ff82ea6693e3ae14d0f32650
Anish.Chhetri <chhetri.anish14@gmail.com>
2026-09-25 21:06:21 +0530

    setup folder structure and app code

--- GameStore.React/src/main.tsx
+++ GameStore.React/src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App.tsx'
+import Home from './pages/Home.tsx';
+import EditGame from './pages/EditGame.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+      <Router>
+          <Routes>
+              <Route path="/" element={<App />}>
+                  <Route index element={<Home />} />
+                  <Route path="editgame" element={<EditGame />} />
+                  <Route path="editgame/:id" element={<EditGame />} />
+              </Route>
+          </Routes>
+      </Router>
     <App />
   </StrictMode>,
 )
